59f1bd8f77d1ba40acdce7d4b2cd7c4d338db5f5
Dmitry Zakharov <zakharov@ie.ibm.com>
2020-05-16 03:20:36 +0100

    tests

--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@ echo ""
 echo native
 time ./hello 
 echo ""
-echo non-native 
-time ~/prg/graalvm/bin/java -cp . Hello
-echo "" 
+if [ -x ~/prg/graalvm/bin/java ] ; then 
+   echo non-native 
+   time ~/prg/graalvm/bin/java -cp . Hello
+   echo "" 
+fi
 echo ""
 echo non-native 
 time java -cp . Hello
--- test2.sh
+++ test2.sh
@@ -4,9 +4,11 @@ echo ""
 echo native
 time ./hello $n
 echo ""
-echo non-native 
-time ~/prg/graalvm/bin/java -cp . Hello $n
-echo "" 
+if [ -x ~/prg/graalvm/bin/java ] ; then 
+   echo non-native 
+   time ~/prg/graalvm/bin/java -cp . Hello $n
+   echo "" 
+fi
 echo ""
 echo non-native 
 time java -cp . Hello $n
